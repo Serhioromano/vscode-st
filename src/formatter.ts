b1ff4e02136161fa46765fa410966192aaa27fc7
Sergey Romanov <serhioromano@outlook.com>
2020-09-11 20:14:00 +0600

    new version

--- src/formatter.ts
+++ src/formatter.ts
@@ -6,28 +6,81 @@ export class STFormatterProvider implements vscode.DocumentFormattingEditProvide
     public out: Array<vscode.TextEdit> = [];
 
     provideDocumentFormattingEdits(document: vscode.TextDocument) {
+        if (vscode.window.visibleTextEditors.every((e) => e.document.fileName !== document.fileName)) {
+            return [];
+        }
+
+        this.out = [];
+
         this.capitalize(document);
 
         return this.out;
     }
 
     capitalize(document: vscode.TextDocument) {
-        let keywords = ['true', 'false', 'exit', 'continue', 'return', 'constant', 'retain'];
+        let text = document.getText();
 
-        for (let line = 0; line < document.lineCount; line++) {
-            const element = document.lineAt(line);
-            for (let i = 0; i < keywords.length; i++) {
-                let str = keywords[i];
-                let last_char = 0;
-                while (element.text.indexOf(str, last_char) >= 0) {
-                    let char = element.text.indexOf(str, last_char);
-                    last_char = char + str.length;
-                    this.out.push(vscode.TextEdit.replace(new vscode.Range(
-                        new vscode.Position(line, char),
-                        new vscode.Position(line, last_char)
-                    ), str.toUpperCase()));
-                }
-            }
-        }
+        let keywords = [
+            'true', 'false', 'exit', 'continue', 'return', 'constant', 'retain',
+            'public', 'private', 'protected', 'abstract','persistent','internal',
+            'final','of','else','elsif','then','__try','__catch','__finally',
+            '__endtry','do','to','by','task','with','using','uses','from',
+            'until','or','or_else','and','and_then','not','xor','nor','ge',
+            'le','eq','ne','gt','lt','__new','__delete', 'extends','implements',
+            'this','super'
+        ];
+        let regEx = new RegExp(`\\b(?:${keywords.join('|')}|AT|BOOL|BYTE|(?:D|L)?WORD|U?(?:S|D|L)?INT|L?REAL|TIME(?:_OF_DAY)?|TOD|DT|DATE(?:_AND_TIME)?|STRING|ARRAY|ANY)\\b`, "ig");
+        text = text.replace(regEx, (match) => {
+            return match.toUpperCase();
+        });
+        
+        regEx = new RegExp("\\b[A-Za-z_]*(_TO_)[A-Za-z_]*\\b", "ig");
+        text = text.replace(regEx, (match) => {
+            return match.toUpperCase();
+        });
+        regEx = new RegExp("\\b(TO_|FROM_|TRUNC_)[A-Za-z_]*\\b", "ig");
+        text = text.replace(regEx, (match) => {
+            return match.toUpperCase();
+        });
+
+        let end = [
+            'var', 'program', 'if', 'case', 'while', 'for', 'repeat', 'function',
+            'function_block', 'struct', 'configuration', 'tcp', 'resource',
+            'channel', 'library', 'folder', 'binaries', 'includes', 'sources',
+            'action', 'step', 'initial_step', 'transition', 'type', 'namespace',
+            'implementation', 'interface', 'property', 'method'
+        ];
+        regEx = new RegExp(`\\b(?:END_)?(?:${end.join('|')})\\b`, "ig");
+        text = text.replace(regEx, (match) => {
+            return match.toUpperCase();
+        });
+
+        let functions = [
+            'abs', 'sin', 'mod', 'abs', 'acos', 'asin', 'atan', 'cos', 'exp',
+            'expt', 'ln', 'log', 'sin', 'sqrt', 'tan', 'sel', 'mux', 'shl',
+            'shr', 'rol', 'ror', 'add', 'div', 'mul', 'sub', 'limit', 'max',
+            'min', 'adr', 'adrinst', 'size', 'sizeof', 'bit_adr', 'bit_trunc',
+            'rs', 'sr', 'ton', 'tp', 'tof', 'trunc', 'ctd', 'ctu', 'ctud', 'r_trig',
+            'f_trig', 'move', 'concat', 'delete', 'find', 'insert', 'left', 'len',
+            'replace', 'right', 'rtc', 'mid', 'sema', 'round', 'floor', 'ceil',
+            'unpack', 'ref'
+        ];
+
+        regEx = new RegExp(`\\b(?:${functions.join('|')})\\b\\s+\\(`, "ig");
+        text = text.replace(regEx, (match) => {
+            return match.replace(/\s+/, '');
+        });
+
+        regEx = new RegExp(`\\b(?:${functions.join('|')})\\(`, "ig");
+        text = text.replace(regEx, (match) => {
+            return match.toUpperCase();
+        });
+
+        this.out.push(
+            new vscode.TextEdit(new vscode.Range(
+                new vscode.Position(0, 0),
+                document.lineAt(document.lineCount - 1).range.end), text
+            )
+        );
     }
 }
