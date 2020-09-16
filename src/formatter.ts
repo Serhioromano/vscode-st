4a74ddfc5ad94f963272d257ffb0d165370a55a5
Sergey Romanov <serhioromano@outlook.com>
2020-09-16 13:10:03 +0600

    new formatting regex

--- src/formatter.ts
+++ src/formatter.ts
@@ -22,26 +22,18 @@ export class STFormatterProvider implements vscode.DocumentFormattingEditProvide
 
         let keywords = [
             'true', 'false', 'exit', 'continue', 'return', 'constant', 'retain',
-            'public', 'private', 'protected', 'abstract','persistent','internal',
-            'final','of','else','elsif','then','__try','__catch','__finally',
-            '__endtry','do','to','by','task','with','using','uses','from',
-            'until','or','or_else','and','and_then','not','xor','nor','ge',
-            'le','eq','ne','gt','lt','__new','__delete', 'extends','implements',
-            'this','super'
+            'public', 'private', 'protected', 'abstract', 'persistent', 'internal',
+            'final', 'of', 'else', 'elsif', 'then', '__try', '__catch', '__finally',
+            '__endtry', 'do', 'to', 'by', 'task', 'with', 'using', 'uses', 'from',
+            'until', 'or', 'or_else', 'and', 'and_then', 'not', 'xor', 'nor', 'ge',
+            'le', 'eq', 'ne', 'gt', 'lt', '__new', '__delete', 'extends', 'implements',
+            'this', 'super'
         ];
-        let regEx = new RegExp(`\\b(?:${keywords.join('|')}|AT|BOOL|BYTE|(?:D|L)?WORD|U?(?:S|D|L)?INT|L?REAL|TIME(?:_OF_DAY)?|TOD|DT|DATE(?:_AND_TIME)?|STRING|ARRAY|ANY)\\b`, "ig");
-        text = text.replace(regEx, (match) => {
-            return match.toUpperCase();
-        });
-        
-        regEx = new RegExp("\\b[A-Za-z_]*(_TO_)[A-Za-z_]*\\b", "ig");
-        text = text.replace(regEx, (match) => {
-            return match.toUpperCase();
-        });
-        regEx = new RegExp("\\b(TO_|FROM_|TRUNC_)[A-Za-z_]*\\b", "ig");
-        text = text.replace(regEx, (match) => {
-            return match.toUpperCase();
-        });
+
+        let types = [
+            'AT', 'BOOL', 'BYTE', '(?:D|L)?WORD', 'U?(?:S|D|L)?INT', 'L?REAL', 
+            'TIME(?:_OF_DAY)?', 'TOD', 'DT', 'DATE(?:_AND_TIME)?', 'W?STRING', 
+            'ARRAY', 'ANY'];
 
         let end = [
             'var', 'program', 'if', 'case', 'while', 'for', 'repeat', 'function',
@@ -50,10 +42,6 @@ export class STFormatterProvider implements vscode.DocumentFormattingEditProvide
             'action', 'step', 'initial_step', 'transition', 'type', 'namespace',
             'implementation', 'interface', 'property', 'method'
         ];
-        regEx = new RegExp(`\\b(?:END_)?(?:${end.join('|')})\\b`, "ig");
-        text = text.replace(regEx, (match) => {
-            return match.toUpperCase();
-        });
 
         let functions = [
             'abs', 'sin', 'mod', 'abs', 'acos', 'asin', 'atan', 'cos', 'exp',
@@ -63,17 +51,21 @@ export class STFormatterProvider implements vscode.DocumentFormattingEditProvide
             'rs', 'sr', 'ton', 'tp', 'tof', 'trunc', 'ctd', 'ctu', 'ctud', 'r_trig',
             'f_trig', 'move', 'concat', 'delete', 'find', 'insert', 'left', 'len',
             'replace', 'right', 'rtc', 'mid', 'sema', 'round', 'floor', 'ceil',
-            'unpack', 'ref'
-        ];
+            'unpack', 'ref',
 
-        regEx = new RegExp(`\\b(?:${functions.join('|')})\\b\\s+\\(`, "ig");
+            '[A-Za-z_]*(_TO_)[A-Za-z_]*', '(?:TO_|FROM_|TRUNC_)[A-Za-z_]*',
+            ''
+        ];
+        
+        let regEx = new RegExp(`\\b(?:${functions.join('|')})\\b\\s+\\(`, "ig");
         text = text.replace(regEx, (match) => {
             return match.replace(/\s+/, '');
         });
 
-        regEx = new RegExp(`\\b(?:${functions.join('|')})\\(`, "ig");
-        text = text.replace(regEx, (match) => {
-            return match.toUpperCase();
+
+        regEx = new RegExp(`(?<!\\\\(?:\\\\{2})*)["']{1}[^"'\\\\]*(?:\\\\[\\s\\S][^\\\\"']*)*["']{1}|\\(\\*[\\s\\S]*?\\*\\)|\\/\\*[\\s\\S]*?\\*\\/|\\/\\/[^\\n]*\\n|\\b(${types.join('|')}|${keywords.join('|')}|(?:END_)?(?:${end.join('|')})|${functions.join('\\(|')}\\()\\b`, "ig");
+        text = text.replace(regEx, (match, group) => {
+            return group != undefined ? match.toUpperCase() : match;
         });
 
         this.out.push(
