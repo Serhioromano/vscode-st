70b86e9d9225183748bcc67ecb50c97e085eff46
serhioromano <serhioromano@outlook.com>
2022-02-08 22:50:10 +0600

    Work on LSP

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -11,48 +11,43 @@ import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } f
 let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider("st", new STFormatterProvider()));
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider("st", new STFormatterProvider()));
 
-	context.subscriptions.push(
-		vscode.languages.registerDocumentSymbolProvider({ language: "st" }, new STDocumentSymbolProvider())
-	);
-	// The server is implemented in node
-	const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+    context.subscriptions.push(
+        vscode.languages.registerDocumentSymbolProvider({ language: "st" }, new STDocumentSymbolProvider())
+    );
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	const serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions,
-		},
-	};
+    const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
+    const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
-	// Options to control the language client
-	const clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: "file", language: "plaintext" }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: vscode.workspace.createFileSystemWatcher("**/.clientrc"),
-		},
-	};
+    const serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions,
+        },
+    };
 
-	// Create the language client and start the client.
-	client = new LanguageClient("languageServerExample", "Language Server Example", serverOptions, clientOptions);
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: [{ scheme: "file", language: "st" }],
+        synchronize: {
+            fileEvents: vscode.workspace.createFileSystemWatcher("**/.clientrc"),
+        },
+    };
 
-	// Start the client. This will also launch the server
-	client.start();
+    client = new LanguageClient(
+        "STlanguageServer",
+        "Language Server for Structured Text",
+        serverOptions,
+        clientOptions
+    );
+    client.start();
 }
 
 export function deactivate() {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -10,215 +10,219 @@ import {
     CompletionItemKind,
     TextDocumentPositionParams,
     TextDocumentSyncKind,
-    InitializeResult
-  } from 'vscode-languageserver/node';
-  
-  import { TextDocument } from 'vscode-languageserver-textdocument';
-  
-  // Create a connection for the server, using Node's IPC as a transport.
-  // Also include all preview / proposed LSP features.
-  let connection = createConnection(ProposedFeatures.all);
-  
-  // Create a simple text document manager.
-  let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
-  
-  let hasConfigurationCapability: boolean = false;
-  let hasWorkspaceFolderCapability: boolean = false;
-  let hasDiagnosticRelatedInformationCapability: boolean = false;
-  
-  connection.onInitialize((params: InitializeParams) => {
+    InitializeResult,
+} from "vscode-languageserver/node";
+
+import { TextDocument } from "vscode-languageserver-textdocument";
+
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+let connection = createConnection(ProposedFeatures.all);
+
+// Create a simple text document manager.
+let documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
+let hasConfigurationCapability: boolean = false;
+let hasWorkspaceFolderCapability: boolean = false;
+let hasDiagnosticRelatedInfoCapability: boolean = false;
+
+// The example settings
+interface STSettings {
+    maxNumberOfProblems: number;
+    fileExtensions: string[];
+}
+
+const defaultSettings: STSettings = {
+    maxNumberOfProblems: 1000,
+    fileExtensions: [".st", ".scl", ".exp", ".iecplc", ".iecst", ".TcDUT", ".TcPOU", ".iecstl"]
+};
+let globalSettings: STSettings = defaultSettings;
+let documentSettings: Map<string, Thenable<STSettings>> = new Map();
+
+connection.onInitialize((params: InitializeParams) => {
     let capabilities = params.capabilities;
-  
-    // Does the client support the `workspace/configuration` request?
-    // If not, we fall back using global settings.
-    hasConfigurationCapability = !!(
-      capabilities.workspace && !!capabilities.workspace.configuration
-    );
-    hasWorkspaceFolderCapability = !!(
-      capabilities.workspace && !!capabilities.workspace.workspaceFolders
-    );
-    hasDiagnosticRelatedInformationCapability = !!(
-      capabilities.textDocument &&
-      capabilities.textDocument.publishDiagnostics &&
-      capabilities.textDocument.publishDiagnostics.relatedInformation
-    );
-  
+
+    hasConfigurationCapability = !!capabilities.workspace?.configuration;
+    hasWorkspaceFolderCapability = !!capabilities.workspace?.workspaceFolders;
+    hasDiagnosticRelatedInfoCapability = !!capabilities.textDocument?.publishDiagnostics?.relatedInformation;
+
     const result: InitializeResult = {
-      capabilities: {
-        textDocumentSync: TextDocumentSyncKind.Incremental,
-        // Tell the client that this server supports code completion.
-        completionProvider: {
-          resolveProvider: true
-        }
-      }
+        capabilities: {
+            textDocumentSync: TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true,
+            }
+        },
     };
     if (hasWorkspaceFolderCapability) {
-      result.capabilities.workspace = {
-        workspaceFolders: {
-          supported: true
-        }
-      };
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true,
+            },
+        };
     }
     return result;
-  });
-  
-  connection.onInitialized(() => {
+});
+
+connection.onInitialized(async () => {
     if (hasConfigurationCapability) {
-      // Register for all configuration changes.
-      connection.client.register(DidChangeConfigurationNotification.type, undefined);
+        // Register for all configuration changes.
+        connection.client.register(DidChangeConfigurationNotification.type, undefined);
     }
     if (hasWorkspaceFolderCapability) {
-      connection.workspace.onDidChangeWorkspaceFolders(_event => {
-        connection.console.log('Workspace folder change event received.');
-      });
+        connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+            parseAllFilesInWorkspaceFolder();
+            connection.console.log("Workspace folder change event received.");
+        });
+        parseAllFilesInWorkspaceFolder();
     }
-  });
-  
-  // The example settings
-  interface ExampleSettings {
-    maxNumberOfProblems: number;
-  }
-  
-  // The global settings, used when the `workspace/configuration` request is not supported by the client.
-  // Please note that this is not the case when using this server with the client provided in this example
-  // but could happen with other clients.
-  const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-  let globalSettings: ExampleSettings = defaultSettings;
-  
-  // Cache the settings of all open documents
-  let documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
-  
-  connection.onDidChangeConfiguration(change => {
+    function parseAllFilesInWorkspaceFolder() {
+        connection.workspace.getWorkspaceFolders().then(folders => {
+            if (folders === null) {
+                return;
+            }
+            getDocumentSettings().then(settings => {
+                folders.forEach(folder => {
+                    settings.fileExtensions.forEach(ext => {
+    
+                    });
+                });
+            });
+        });
+    }
+});
+
+
+connection.onDidChangeConfiguration((change) => {
     if (hasConfigurationCapability) {
-      // Reset all cached document settings
-      documentSettings.clear();
+        documentSettings.clear();
     } else {
-      globalSettings = <ExampleSettings>(
-        (change.settings.languageServerExample || defaultSettings)
-      );
+        globalSettings = <STSettings>(change.settings.STLSP || defaultSettings);
     }
-  
+
     // Revalidate all open text documents
     documents.all().forEach(validateTextDocument);
-  });
-  
-  function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
+});
+
+function getDocumentSettings(resource?: string): Thenable<STSettings> {
     if (!hasConfigurationCapability) {
-      return Promise.resolve(globalSettings);
+        return Promise.resolve(globalSettings);
+    }
+    if (resource === undefined) {
+        return connection.workspace.getConfiguration({
+            section: "STLSP",
+        });
     }
     let result = documentSettings.get(resource);
     if (!result) {
-      result = connection.workspace.getConfiguration({
-        scopeUri: resource,
-        section: 'languageServerExample'
-      });
-      documentSettings.set(resource, result);
+        result = connection.workspace.getConfiguration({
+            scopeUri: resource,
+            section: "STLSP",
+        });
+        documentSettings.set(resource, result);
     }
     return result;
-  }
-  
-  // Only keep settings for open documents
-  documents.onDidClose(e => {
+}
+
+// Only keep settings for open documents
+documents.onDidClose((e) => {
     documentSettings.delete(e.document.uri);
-  });
-  
-  // The content of a text document has changed. This event is emitted
-  // when the text document first opened or when its content has changed.
-  documents.onDidChangeContent(change => {
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent((change) => {
     validateTextDocument(change.document);
-  });
-  
-  async function validateTextDocument(textDocument: TextDocument): Promise<void> {
+});
+
+async function validateTextDocument(textDocument: TextDocument): Promise<void> {
     // In this simple example we get the settings for every validate run.
     let settings = await getDocumentSettings(textDocument.uri);
-  
+
     // The validator creates diagnostics for all uppercase words length 2 and more
     let text = textDocument.getText();
     let pattern = /\b[A-Z]{2,}\b/g;
     let m: RegExpExecArray | null;
-  
+
     let problems = 0;
     let diagnostics: Diagnostic[] = [];
     while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-      problems++;
-      let diagnostic: Diagnostic = {
-        severity: DiagnosticSeverity.Warning,
-        range: {
-          start: textDocument.positionAt(m.index),
-          end: textDocument.positionAt(m.index + m[0].length)
-        },
-        message: `${m[0]} is all uppercase.`,
-        source: 'ex'
-      };
-      if (hasDiagnosticRelatedInformationCapability) {
-        diagnostic.relatedInformation = [
-          {
-            location: {
-              uri: textDocument.uri,
-              range: Object.assign({}, diagnostic.range)
+        problems++;
+        let diagnostic: Diagnostic = {
+            severity: DiagnosticSeverity.Warning,
+            range: {
+                start: textDocument.positionAt(m.index),
+                end: textDocument.positionAt(m.index + m[0].length),
             },
-            message: 'Spelling matters'
-          },
-          {
-            location: {
-              uri: textDocument.uri,
-              range: Object.assign({}, diagnostic.range)
-            },
-            message: 'Particularly for names'
-          }
-        ];
-      }
-      diagnostics.push(diagnostic);
+            message: `${m[0]} is all uppercase.`,
+            source: "ex",
+        };
+        if (hasDiagnosticRelatedInfoCapability) {
+            diagnostic.relatedInformation = [
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: "Spelling matters",
+                },
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: "Particularly for names",
+                },
+            ];
+        }
+        diagnostics.push(diagnostic);
     }
-  
+
     // Send the computed diagnostics to VS Code.
     connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-  }
-  
-  connection.onDidChangeWatchedFiles(_change => {
+}
+
+connection.onDidChangeWatchedFiles((_change) => {
     // Monitored files have change in VS Code
-    connection.console.log('We received a file change event');
-  });
-  
-  // This handler provides the initial list of the completion items.
-  connection.onCompletion(
-    (_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-      // The pass parameter contains the position of the text document in
-      // which code complete got requested. For the example we ignore this
-      // info and always provide the same completion items.
-      return [
+    connection.console.log("We received a file change event");
+});
+
+// This handler provides the initial list of the completion items.
+connection.onCompletion((_textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
+    // The pass parameter contains the position of the text document in
+    // which code complete got requested. For the example we ignore this
+    // info and always provide the same completion items.
+    return [
         {
-          label: 'TypeScript',
-          kind: CompletionItemKind.Text,
-          data: 1
+            label: "TypeScript",
+            kind: CompletionItemKind.Text,
+            data: 1,
         },
         {
-          label: 'JavaScript',
-          kind: CompletionItemKind.Text,
-          data: 2
-        }
-      ];
+            label: "JavaScript",
+            kind: CompletionItemKind.Text,
+            data: 2,
+        },
+    ];
+});
+
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+    if (item.data === 1) {
+        item.detail = "TypeScript details";
+        item.documentation = "TypeScript documentation";
+    } else if (item.data === 2) {
+        item.detail = "JavaScript details";
+        item.documentation = "JavaScript documentation";
     }
-  );
-  
-  // This handler resolves additional information for the item selected in
-  // the completion list.
-  connection.onCompletionResolve(
-    (item: CompletionItem): CompletionItem => {
-      if (item.data === 1) {
-        item.detail = 'TypeScript details';
-        item.documentation = 'TypeScript documentation';
-      } else if (item.data === 2) {
-        item.detail = 'JavaScript details';
-        item.documentation = 'JavaScript documentation';
-      }
-      return item;
-    }
-  );
-  
-  // Make the text document manager listen on the connection
-  // for open, change and close text document events
-  documents.listen(connection);
-  
-  // Listen on the connection
-  connection.listen();
+    return item;
+});
+
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// Listen on the connection
+connection.listen();
